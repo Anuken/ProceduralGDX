0a0cfe8e4ad7892dae90f992d630c6a07848c2df
Anuken <arnukren@gmail.com>
2017-11-14 20:36:35 -0500

    Added line generator

--- core/src/io/anuke/procgdx/generators/CanyonGenerator.java
+++ core/src/io/anuke/procgdx/generators/CanyonGenerator.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Array;
 
 import io.anuke.ucore.graphics.Hue;
 import io.anuke.ucore.util.Angles;
+import io.anuke.ucore.util.Mathf;
 import io.anuke.ucore.util.Tmp;
 
 public class CanyonGenerator extends BasicNoise{
@@ -19,20 +20,33 @@ public class CanyonGenerator extends BasicNoise{
 	double radius = 0.1;
 	double linepow = 1.2;
 	double nscl = 0.5;
+	int type = 2; //0 = circle, 1 = cross, 2 = jagged
 
 	public CanyonGenerator() {
-		//lines.add(new Line2D.Float(0, 0, 1f, 1f));
-		//lines.add(new Line2D.Float(1, 0, 0f, 1f));
+		if(type == 0){
+			int sides = 20;
+			float rad = 0.3f;
 
-		int sides = 20;
-		float rad = 0.4f;
-
-		for(int i = 0; i < sides; i++){
-			Angles.translation(i * 360f / sides, rad);
-			float x1 = Angles.x(), y1 = Angles.y();
-			Angles.translation((i + 1) * 360f / sides, rad);
-			lines.add(new Line2D.Float(x1 + 0.5f, y1 + 0.5f, Angles.x() + 0.5f, Angles.y() + 0.5f));
+			for(int i = 0; i < sides; i++){
+				Angles.translation(i * 360f / sides, rad);
+				float x1 = Angles.x(), y1 = Angles.y();
+				Angles.translation((i + 1) * 360f / sides, rad);
+				lines.add(new Line2D.Float(x1 + 0.5f, y1 + 0.5f, Angles.x() + 0.5f, Angles.y() + 0.5f));
+			}
+		}else if(type == 1){
+			lines.add(new Line2D.Float(0, 0, 1f, 1f));
+			lines.add(new Line2D.Float(1, 0, 0f, 1f));
+		}else{
+			float lastx = 0, lasty = 0;
+			int linecount = 30;
+			for(int i = 0; i < linecount; i ++){
+				float nx = lastx + Mathf.random(2f/linecount), ny = lasty + Mathf.random(2f/linecount);
+				lines.add(new Line2D.Float(lastx, lasty, nx, ny));
+				lastx = nx;
+				lasty = ny;
+			}
 		}
+
 	}
 
 	@Override
@@ -42,9 +56,11 @@ public class CanyonGenerator extends BasicNoise{
 			double noise = sim.octaveNoise2D(octaves, falloff, 1 / scale / ((float) size / 400f), x, y) * nscl;
 
 			double min = 1f;
+			float s1 = 0.5f, s2 = 0.3f;
 
 			for(Line2D.Float line : lines){
-				double dst = Math.pow(dist(line.x1, line.y1, line.x2, line.y2, (float) x / size, (float) y / size), linepow) / radius + noise;
+				double dst = (Math.pow(dist(line.x1, line.y1, line.x2, line.y2, (float) x / size, (float) y / size), linepow) 
+						/ radius + noise*s1) * Mathf.lerp((float) noise, 1f, s2);
 				min = Math.min(dst, min);
 			}
 
